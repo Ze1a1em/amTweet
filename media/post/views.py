c8e4a4cfbe40619a988aca389fc3ea42faab2966
ZS <zelalemsmith@Zelalems-MacBook-Air.local>
2022-04-12 16:01:04 -0500

    created the app 'post' and started a react app as the 'frontend'.

---
+++ media/post/views.py
@@ -0,0 +1,9 @@
+import imp
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .models import UserSerializer
+# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
